6e7400034b494796b60669c27ebafd8eeae11aa0
lucasdina <lukeadina@gmail.com>
2018-01-16 17:48:55 -0700

    Added w1ch1.php for daily lab.

---
+++ htdocs/book_apps/ch01_product_discount/wk1ch1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+    <link rel="stylesheet" type="text/css"
+          href="main.css"/>
+</head>
+<body>
+
+<?php
+$txt = "Lucas's Hello world!";  //Change Mickey to your name
+
+echo "<h2>";
+echo $txt;
+echo "</h2>";
+?>
+</body>
+</html>
