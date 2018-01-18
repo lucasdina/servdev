ea4f77ad557148d63fc5150340e03bf8a0aaadf5
lucasdina <lukeadina@gmail.com>
2018-01-17 17:02:43 -0700

    Added portfolio for project 1.

--- htdocs/ex_starts/ch02_ex1/display_discount.php
+++ htdocs/ex_starts/ch02_ex1/display_discount.php
@@ -3,25 +3,33 @@
 <head>
     <title>Product Discount Calculator</title>
     <link rel="stylesheet" type="text/css" href="main.css">
+    <?php
+    $desc =  $_POST['product_description'];
+    $price =  $_POST['list_price'];
+    $discount =  $_POST['discount_percent'];
+    $amtOff = ($discount/100)*$price;
+    $amtTtl = ($price - $amtOff);
+
+    ?>
 </head>
 <body>
     <main>
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $desc; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $price; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount.'%'; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $amtOff; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $amtTtl; ?></span><br>
     </main>
 </body>
 </html>
