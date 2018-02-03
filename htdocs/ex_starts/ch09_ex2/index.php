1a6ce0ec360cde0f6d10e2f34bb10a3fdfb1e884
lucasdina <lukeadina@gmail.com>
2018-02-03 12:51:30 -0700

    ch11 work was added.

--- htdocs/ex_starts/ch09_ex2/index.php
+++ htdocs/ex_starts/ch09_ex2/index.php
@@ -5,6 +5,11 @@ $number2 = -105.33;
 $number3 = 0.0049;
 $message = 'Enter some numbers and click on the Submit button.';
 
+//Function to check if a field is null or empty.
+function IsNullOrEmptyString($field){
+    return (!isset($field) || trim($field)==='');
+}
+
 //process
 $action = filter_input(INPUT_POST, 'action');
 switch ($action) {
@@ -12,35 +17,51 @@ switch ($action) {
         $number1 = filter_input(INPUT_POST, 'number1');
         $number2 = filter_input(INPUT_POST, 'number2');
         $number3 = filter_input(INPUT_POST, 'number3');
+        $numArray = [$number1, $number2, $number3];
+
 
         // make sure the user enters three numbers
+        if(IsNullOrEmptyString($number1)){$message = "Number 1 can't be empty or null."; break;}
+        if(IsNullOrEmptyString($number2)){$message = "Number 2 can't be empty or null."; break;}
+        if(IsNullOrEmptyString($number3)){$message = "Number 3 can't be empty or null."; break;}
 
         // make sure the numbers are valid
+        if(!is_numeric($number1)){$message = "Number 1 must be numeric."; break;}
+        if(!is_numeric($number2)){$message = "Number 2 must be numeric."; break;}
+        if(!is_numeric($number3)){$message = "Number 3 must be numeric."; break;}
 
         // get the ceiling and floor for $number2
+        $number2_floor = floor($number2);
+        $number2_ceil = ceil($number2);
 
         // round $number3 to 3 decimal places
+        $number3_rounded = round($number3, 3);
 
         // get the max and min values of all three numbers
+        $min = 0;
+        $max = 0;
+        foreach ($numArray as $num){
+            if ($num > $max) $max = $num;
+            if ($num < $max) $min = $num;
+        }
 
         // generate a random integer between 1 and 100
+        $random = rand(0, 100);
 
         // format the message
-        $message = "This page is under construction.\n" .
-                   "Please write the code that process the data.";
-        //$message =
-        //    "Number 1: $number1\n" .
-        //    "Number 2: $number2\n" .
-        //    "Number 3: $number3\n" .
-        //    "\n" .
-        //    "Number 2 ceiling: $number2_ceil\n" .
-        //    "Number 2 floor: $number2_floor\n" .
-        //    "Number 3 rounded: $number3_rounded\n" .
-        //    "\n" .
-        //    "Min: $min\n" .
-        //    "Max: $max\n" .
-        //    "\n" .
-        //    "Random: $random\n";
+        $message =
+            "Number 1: $number1\n" .
+            "Number 2: $number2\n" .
+            "Number 3: $number3\n" .
+            "\n" .
+            "Number 2 ceiling: $number2_ceil\n" .
+            "Number 2 floor: $number2_floor\n" .
+            "Number 3 rounded: $number3_rounded\n" .
+            "\n" .
+            "Min: $min\n" .
+            "Max: $max\n" .
+            "\n" .
+            "Random: $random\n";
 
         break;
 }
--- htdocs/ex_starts/ch10_ex1/index.php
+++ htdocs/ex_starts/ch10_ex1/index.php
@@ -1,11 +1,17 @@
 <?php
 //set default value
 $message = '';
+date_default_timezone_set('America/Denver');
 
 //get value from POST array
 $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
     $action =  'start_app';
+}
+
+//Function to check if a field is null or empty.
+function IsNullOrEmptyString($field){
+    return (!isset($field) || trim($field)==='');
 }
 
 //process
@@ -31,24 +37,78 @@ switch ($action) {
         $due_date_s = filter_input(INPUT_POST, 'due_date');
 
         // make sure the user enters both dates
+        if(IsNullOrEmptyString($invoice_date_s)){
+            $invoice_date_f = 'Field cannot be empty.';
+            $due_date_f = "";
+            $current_date_f = "";
+            $current_time_f = "";
+            $due_date_message = "";
+            break;
+        }
+        if(IsNullOrEmptyString($due_date_s)){
+            $due_date_f = 'Field cannot be empty.';
+            $invoice_date_f = "";
+            $current_date_f = "";
+            $current_time_f = "";
+            $due_date_message = "";
+            break;
+        }
 
         // convert date strings to DateTime objects
         // and use a try/catch to make sure the dates are valid
+        // format both dates
+        try{
+            $invoice_date_f = date("l jS, Y", strtotime($invoice_date_s));
+        } catch(Exception $e){
+            $invoice_date_f = "Date was not formatted correctly.";
+            $due_date_f = "";
+            $current_date_f = "";
+            $current_time_f = "";
+            $due_date_message = "";
+            break;
+        }
+        try{
+            $due_date_f = date("l jS, Y", strtotime($due_date_s));
+        } catch(Exception $e){
+            $due_date_f = "Date was not formatted correctly.";
+            $invoice_date_f = "";
+            $current_date_f = "";
+            $current_time_f = "";
+            $due_date_message = "";
+            break;
+        }
 
         // make sure the due date is after the invoice date
-
-        // format both dates
-        $invoice_date_f = 'not implemented yet';
-        $due_date_f = 'not implemented yet'; 
+        if((strtotime($due_date_s)-strtotime($invoice_date_s))<0){
+            $due_date_message = "Due date must be after the invoice date.";
+            break;
+        }
         
         // get the current date and time and format it
-        $current_date_f = 'not implemented yet';
-        $current_time_f = 'not implemented yet';
-        
+        $current_date_f = date("l jS, Y");
+        $current_time_f = date('g:i:s a');
+        $epochTime = new DateTime('now');
+        $epochTime = date_timestamp_get($epochTime);
         // get the amount of time between the current date and the due date
         // and format the due date message
-        $due_date_message = 'not implemented yet';
-
+        if ($epochTime>strtotime($due_date_s)){
+            $diff = $epochTime-strtotime($due_date_s);
+            $years = floor($diff/31557600);
+            $diff -= ($years*31557600);
+            $months = floor($diff/2629746);
+            $diff -= ($months*2629746);
+            $days = floor($diff/86400);
+            $due_date_message = "Your invoice is ".$years." years, ".$months." months, and ".$days." days overdue.";
+        }
+        else {
+            $diff = strtotime($due_date_s)-$epochTime;
+            $years = floor($diff/31557600);
+            $diff -= ($years*31557600);
+            $months = floor($diff/2629746);
+            $diff -= ($months*2629746);
+            $days = floor($diff/86400);
+            $due_date_message = "Your invoice is due in ".$years." years, ".$months." months, and ".$days." days.";
+        }
         break;
 }
 include 'date_tester.php';
